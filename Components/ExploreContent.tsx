d8bf623e737bd6f799af9efb0013683c8a1f282c
Ayush-Singhal-2004 <ayushsinghal1904@gmail.com>
2024-02-12 11:51:56 +0530

    added the map

--- Components/ExploreContent.tsx
+++ Components/ExploreContent.tsx
@@ -1,29 +1,58 @@
-import { ScrollView, StyleSheet, Dimensions } from "react-native";
+import { ScrollView, StyleSheet, Dimensions, View } from "react-native";
 import Surfing from "./Surfing";
 import AmazingViews from "./AmazingViews";
 import useExploreContent from "../Context/ExploreContext";
 import TinyHomes from "./TinyHomes";
 import AmazingPools from "./AmazingPools"
 import Castles from "./Castles";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import MapView from "react-native-maps";
 
 const windowHeight = Dimensions.get('window').height;
+
 const navOptionContent = [<Surfing />, <AmazingViews />, <TinyHomes />, <AmazingPools />, <Castles />];
 
 export default function ExploreContent() {
 
     const contentContext = useExploreContent();
     const scrollRef = useRef<ScrollView>();
+    const [showMap, setShowMap] = useState(false);
+
     useEffect(() => {
-        scrollRef.current.scrollTo({y : 0, animated : true});
+        if(!showMap)
+        scrollRef.current.scrollTo({y : 2, animated : true});
     });
+
+    const checkScroll = (event:any) => {
+        if(event.nativeEvent.contentOffset.y == 0)
+        {
+            setShowMap(true);
+        }
+    };
     
     return (
-        <ScrollView style={styles.container} ref={scrollRef}>
+        <View>
+        {
+            showMap?
+            <View>
+                <MapView
+                initialRegion={{
+                    latitude: 20.5937,
+                    longitude: 78.9629,
+                    latitudeDelta: 900,
+                    longitudeDelta: 400,
+                  }}
+                style={{height : windowHeight * 68/100}}
+                ></MapView>
+            </View>
+            :
+            <ScrollView style={styles.container} ref={scrollRef} onScroll={checkScroll}>
             {
                 navOptionContent[contentContext.value]
             }
-        </ScrollView>
+            </ScrollView>
+        }
+        </View>
     )
 }
 
